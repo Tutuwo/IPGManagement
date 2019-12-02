8cc53b1cd3d8705a7a044cd62df074857515e5c7
vascoventura <vasco.acv@hotmail.com>
2019-12-02 23:17:03 +0000

    Inicio Bd

--- IPGManagement/Data/IPGManagement.cs
+++ IPGManagement/Data/IPGManagement.cs
@@ -14,6 +14,41 @@ namespace IPGManagement.Models
         }
 
         public DbSet<IPGManagement.Models.Funcionario> Funcionarios { get; set; }
-        public object Funcionario { get; internal set; }
+        public DbSet<IPGManagement.Models.Cargo> Cargos { get; set; }
+
+        public DbSet<IPGManagement.Models.Departamento> Departamentos { get; set; }
+        public DbSet<IPGManagement.Models.Horario> Horarios {get; set;}
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // alternate way of specifying a key with two fields:
+            // https://stackoverflow.com/questions/5962557/multiple-primary-key-with-asp-net-mvc-3
+            modelBuilder.Entity<BookCategory>()
+                .HasKey(bc => new { bc.BookId, bc.CategoryId });
+
+            modelBuilder.Entity<Funcionario>()
+                .HasOne(b => b.Cargo)
+                .WithMany(a => a.Books)
+                .HasForeignKey(b => b.AuthorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // No need for doing this
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(bc => bc.Book)
+                .WithMany(b => b.CategoriesBook)
+                .HasForeignKey(bc => bc.BookId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // No need for doing this
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(bc => bc.Category)
+                .WithMany(c => c.BooksCategory)
+                .HasForeignKey(bc => bc.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
+
+
+}
 }
